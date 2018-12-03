54d0e3a73ef9de93103bd5a516593c806f08c802
IvanKurtovic2000 <ivan.kurtovic@pwc.com>
2018-12-03 08:46:10 -0800

    Java Basics Tutorial

---
+++ src/variables/Variables.java
@@ -0,0 +1,49 @@
+package variables;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		
+		String text = "ABCDEFGHIJKLMN";
+		int zahl1 = 200;
+		int zahl2 = 200;
+		float gleitkommazahl = 102.12f;
+		boolean status = true;
+		char buchstabe = 'G';
+		
+		String text1 = "Hallo ";
+		String text2 = "das ist ein";
+		String text3 = " kurzer text";
+		
+		String fulltext = text1 + text2 + text3;
+		
+		
+		//einfache Ausgabe von Variablen
+		System.out.println("Ausgabe der Variable text: " + text); 
+		System.out.println("Ausgabe der Variable zahl: " + zahl1); 
+		System.out.println("Ausgabe der Variable gleitkommazahl: " + gleitkommazahl); 
+		System.out.println("Ausgabe der Variable status: " + status); 
+		System.out.println("Ausgabe der Variable buchstabe: " + buchstabe); 
+		System.out.println("Ausgabe der Variable fulltext: " + fulltext); 
+		
+		// Ausgabe der Varibale Variable text in Kleinschrift
+	     System.out.println("Ausgabe der Variable text in Kleinschrift: " + text.toLowerCase()); 
+	     
+	     //Ausgabe der Anzahl der Buchstaben von der Variable text: 
+	     System.out.println("Ausgabe der Anzahl der Buchstaben von der Variable text: " + text.length()); 
+	     
+	     //Ausgabe des Buchstaben an der 5 Stelle von der Variable text: 
+	     System.out.println("Ausgabe des Buchstaben an der 5 Stelle von der Variable text: " +  text.charAt(5)); 
+	     
+	     
+	    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	   ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	     
+	   //Rechnen mit Variablen
+	     
+	     
+	     
+
+	}
+
+}
